141b61d914827a9cdef2051dc5a33e900520204b
Victor Lima <victor-lima26@hotmail.com>
2022-01-26 20:22:22 -0300

    refactor: add providers,header and modal to _app page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,21 @@
 import type { AppProps } from 'next/app';
+import { Header } from '../components/Header';
+import { Modal } from '../components/Modal';
+import { ModalProvider } from '../contexts/Modal';
+import { TransactionsProvider } from '../contexts/Transactions';
+import { GlobalStyle } from '../styles/global';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <TransactionsProvider>
+      <ModalProvider>
+        <Header />
+        <Component {...pageProps} />
+        <GlobalStyle />
+        <Modal />
+      </ModalProvider>
+    </TransactionsProvider>
+  );
 }
 
 export default MyApp;
